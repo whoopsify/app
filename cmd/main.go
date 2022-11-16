730a9b83946556485aea342aefd0b59b1b991c78
Andrew <andrew@gitleaks.io>
2022-11-16 00:33:57 -0700

    * Removed secrets

--- cmd/main.go
+++ cmd/main.go
@@ -8,8 +8,6 @@ import (
 func main() {
 	// TODO: Create AWS session
 	fmt.Println("Authenticating to AWS...")
-	aws_token := "AKIAIMNOJVGFDXXXE4OA"
-	awsSession := awsSdk.NewSession(aws_token)
 
 	// TODO: Create AWS S3 buckets
 
